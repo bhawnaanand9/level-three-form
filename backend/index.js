b1e0e3597a827eecf8492b011a900dd122dfe243
bhawnaanand9 <147309536+bhawnaanand9@users.noreply.github.com>
2024-06-23 19:45:36 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -2,24 +2,9 @@ const cors = require("cors");
 const express = require("express");
 const app = express();
 const port = 5000;
-// const corsOptions = {
-//   origin: "https://frontend-1voi3eec6-bhawnas-projects-4cdd8807.vercel.app",
-//   methods: ["GET", "POST"],
-// };
-// app.use(cors(corsOptions));
-const allowedOrigins = [
-  "https://frontend-1voi3eec6-bhawnas-projects-4cdd8807.vercel.app",
-  // Add more origins as needed
-];
-
 const corsOptions = {
-  origin: function (origin, callback) {
-    if (allowedOrigins.indexOf(origin) !== -1 || !origin) {
-      callback(null, true);
-    } else {
-      callback(new Error("Not allowed by CORS"));
-    }
-  },
+  origin: "https://frontend-1voi3eec6-bhawnas-projects-4cdd8807.vercel.app",
+  methods: ["GET", "POST"],
 };
 
 app.use(cors(corsOptions));
